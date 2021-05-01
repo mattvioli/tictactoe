5eab5c2436b2ee46ee895b815fea3811d7b8bc6f
Matt <mattvioli@gmail.com>
2021-05-01 16:28:39 +1000

    changed the turn display to represent whos turn it is

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 let gameObject = {
     playerMarker: 'O',
-    nextPlayer: 'X'
+    nextPlayer: 'X',
 }
 
 const allGameDivs = document.querySelectorAll('.game-div')
+const playerMarkerDisplay = document.querySelector('.playerturn span')
+
 allGameDivs.forEach(element => element.addEventListener('click', clickHandler))
 
 function clickHandler(event) {
     event.target.textContent = gameObject.playerMarker
     gameObject.playerMarker = gameObject.nextPlayer
     gameObject.nextPlayer = event.target.textContent
+    playerMarkerDisplay.textContent = gameObject.playerMarker
 }
