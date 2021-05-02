769712c1222cfe490dfee802a7ee45ce625303b7
Matt <mattvioli@gmail.com>
2021-05-02 10:01:30 +1000

    have row win condition

--- main.js
+++ main.js
@@ -35,5 +35,34 @@ function clickHandler(event) {
     gameObject.playerMarker = gameObject.nextPlayer
     gameObject.nextPlayer = event.target.textContent
     playerMarkerDisplay.textContent = gameObject.playerMarker
+    checkWinner()
+
 }
 
+function checkWinner() {
+    for(let i = 0; i < gameObject.gameBoard.length; i++){
+        let winArray = true
+        //rows
+        for(let j = 0; j < gameObject.gameBoard[i].length; j++){
+            if(gameObject.gameBoard[i][j] !== gameObject.nextPlayer)    {
+                winArray = false
+                break;
+            }
+        }
+        //columns
+        for(let j = 0; j < gameObject.gameBoard[i].length; j++){
+            if(gameObject.gameBoard[i][i] !== gameObject.nextPlayer)    {
+                winArray = false
+                break;
+            }
+        }
+        if(winArray)
+        {
+            alert(`${gameObject.nextPlayer} wins!`)
+        }
+    }
+
+}
+
+//okay what do I need to do.
+// if 
